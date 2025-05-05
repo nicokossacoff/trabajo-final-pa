307159931cc18d967bac65dad4755f6bbdc29c01
javier <javier.cordeiro@shiphero.com>
2025-05-05 08:59:25 -0300

    Fastapi and dockerfile

---
+++ app.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+import sqlite3 
+from datetime import datetime
+app= FastAPI()
+
+def sql_query(query):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('/home/javo/Documents/Maestria/MCD06/tp_airflow/airflow.db')
+    cursor = conn.cursor()
+    
+    # Execute the SQL query
+    cursor.execute(query)
+    
+    # Fetch all results
+    rows = cursor.fetchall()
+    
+    # Close the database connection
+    conn.close()
+    
+    return rows
+
+@app.get ("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get ("/recommendations/{ADV}/{model}")
+def get_recommendations(ADV: str, model: str):
+        
+    if model == 'topctr' :
+        table= 'top_ctr_products'
+    elif model == 'topproduct' :
+        table= 'top_products'
+    else:
+        return {"error": "Invalid model specified. Use 'topctr' or 'topproduct'."}
+     
+    date= '2025-05-03' #date.today().strftime('%Y-%m-%d') 
+
+    query= f"""
+    SELECT advertiser_id, product_id FROM {table} WHERE advertiser_id = '{ADV}' AND date = '{date}'
+    """
+    rows=sql_query(query)
+
+    recommendations_data = []
+    for index, row in rows.iterrows():
+        recommendation = {
+            "advertiser_id": row['advertiser_id'],
+            "product_id": row['product_id']
+        }
+        recommendations_data.append(recommendation)
+   
+    return {"recommendations": recommendations_data}
+
+@app.get("/history/{ADV}")
+#get history o f the las 7 days for adv
+def get_history(ADV: str):
+    query= f"""
+    SELECT advertiser_id, product_id, date FROM top_products WHERE advertiser_id = '{ADV}' AND date >= date('now', '-7 days')
+    """
+    rows=sql_query(query)
+    history_data = []
+    for index, row in rows.iterrows():
+        history = {
+            "advertiser_id": row['advertiser_id'],
+            "product_id": row['product_id'],
+            "date": row['date']
+        }
+        history_data.append(history)
+    return {"history": history_data}
+
+
---
+++ dockerfile
@@ -0,0 +1,22 @@
+
+FROM python:3.11-slim
+
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        gcc \
+    && pip install --no-cache-dir -r requirements.txt \
+    && apt-get purge -y gcc \
+    && apt-get autoremove -y && \
+    rm -rf /var/lib/apt/lists/*
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000", "--workers", "2"]
+# --------------------------------------------
