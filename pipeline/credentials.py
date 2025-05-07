15a8c2aceece9089137baa2c62be1bcf187912ed
Nicolás Kossacoff <121921873+nicokossacoff@users.noreply.github.com>
2025-05-07 18:23:07 -0300

    Create credentials.py script

---
+++ pipeline/credentials.py
@@ -0,0 +1,8 @@
+class Credentials:
+  def __init__(self):
+    self.DATABASE = 'postgres'
+    self.USER = 'postgres'
+    self.PASSWORD = 'JHZ1nea@bge8vbw7ymf'
+    self.HOST = '34.173.90.191'
+    self.POST = '5432'
+    self.BUCKET_NAME = 'tp-buckets-prog-avanzada'
